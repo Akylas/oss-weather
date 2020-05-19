0abdc85d7bc7ecfce67205ffbe38e824d04b07e5
Martin Guillon <martin.guillon@akylas.fr>
2020-05-19 11:50:02 +0200

    fix: use canvaslabel

--- app/TopWeatherView.svelte
+++ app/TopWeatherView.svelte
@@ -34,8 +34,8 @@
             parent: this,
             view: AlertView,
             props: {
-                alerts: item.alerts
-            }
+                alerts: item.alerts,
+            },
         });
     }
 
@@ -68,9 +68,7 @@
             if (!chartInitialized) {
                 const darkTheme = /dark/.test(Theme.getMode());
                 const textColor = darkTheme ? 'white' : 'black';
-                const limitColor = Color(textColor)
-                    .setAlpha(0.5)
-                    .toRgbString();
+                const limitColor = Color(textColor).setAlpha(0.5).toRgbString();
                 chartInitialized = true;
                 chart.setNoDataText(null);
                 chart.setAutoScaleMinMaxEnabled(true);
@@ -81,10 +79,10 @@
                 xAxis.setDrawGridLines(false);
                 xAxis.setDrawMarkTicks(true);
                 xAxis.setValueFormatter({
-                    getAxisLabel: f => {
+                    getAxisLabel: (f) => {
                         const result = Math.floor((f - now) / 600000) * 10;
                         return result === 0 ? '' : result + 'm';
-                    }
+                    },
                 });
                 xAxis.setLabelCount(count / 2, true);
                 xAxis.setPosition(XAxisPosition.BOTTOM);
@@ -129,7 +127,7 @@
             }
             let min = 10000;
             let max = -10000;
-            data.forEach(h => {
+            data.forEach((h) => {
                 if (h.precipIntensity < min) {
                     min = h.precipIntensity;
                 }
@@ -142,7 +140,7 @@
             // if (!precipChartSet || !cloudChartSet) {
             let needsToSetData = false;
             let needsUpdate = false;
-            if (data.some(d => d.precipIntensity > 0)) {
+            if (data.some((d) => d.precipIntensity > 0)) {
                 if (!precipChartSet) {
                     needsToSetData = true;
                     // console.log((data.map(s=>s.precipIntensity)));
@@ -164,7 +162,7 @@
             } else if (precipChartSet) {
                 precipChartSet.clear();
             }
-            if (data.some(d => d.cloudCeiling > 0)) {
+            if (data.some((d) => d.cloudCeiling > 0)) {
                 if (!cloudChartSet) {
                     needsToSetData = true;
                     cloudChartSet = new LineDataSet(data, 'cloudCeiling', 'time', 'cloudCeiling');
@@ -184,7 +182,7 @@
                 cloudChartSet.clear();
             }
             if (needsToSetData) {
-                chart.setData(new LineData([precipChartSet, cloudChartSet].filter(s=>!!s))); 
+                chart.setData(new LineData([precipChartSet, cloudChartSet].filter((s) => !!s)));
             } else if (needsUpdate) {
                 chart.getData().notifyDataChanged();
                 chart.notifyDataSetChanged();
@@ -201,93 +199,58 @@
     }
 </script>
 
-<gridLayout rows="auto,auto,auto,auto,*" {height} columns="auto,*">
-    <label marginRight="10" row="0" colSpan="2" fontSize="20" textAlignment="right" verticalTextAlignment="top" text={convertTime(item.time, 'dddd')} />
+<gridLayout rows="auto,*" {height} columns="*,auto">
+<!-- htmllabel 10 more views -->
+<!-- label 25 more views !!! -->
+    <canvaslabel colSpan="2">
+        <cspan id="first" paddingRight="10" fontSize="20" textAlignment="right" verticalAlignment="top" text={convertTime(item.time, 'dddd')} />
 
-    {#if item.temperature !== undefined}
-        <label marginLeft="10" fontSize="12" row="0" verticalTextAlignment="top">
-            <!-- <label fontSize="12" row="0" rowSpan="2" paddingLeft="10" verticalAlignment="top"> -->
-            <span fontSize="26" text={formatValueToUnit(item.temperature, UNITS.Celcius)} />
-            <!-- <span text="({formatValueToUnit(item.temperatureMin, UNITS.Celcius)} | {formatValueToUnit(item.temperatureMax, UNITS.Celcius)}){'\n'}" /> -->
-            <!-- <span fontFamily={mdiFontFamily} text="mdi-hand" /> -->
-            <span color={textLightColor} text={item.temperature !== item.apparentTemperature ? ' ' + formatValueToUnit(item.apparentTemperature, UNITS.Celcius) : ''} />
-            <!-- </label> -->
-        </label>
-    {:else}
-        <label marginLeft="10" fontSize="12" row="0" verticalTextAlignment="top">
-            <!-- <label row="0" rowSpan="2" paddingLeft="10" verticalAlignment="top"> -->
-            <span fontSize="26" text={formatValueToUnit(item.temperatureMin, UNITS.Celcius)} />
-            <span color="#777" fontSize="26" text=" | " />
-            <span fontSize="26" text={formatValueToUnit(item.temperatureMax, UNITS.Celcius)} />
-        </label>
-    {/if}
+        {#if item.temperature !== undefined}
+            <cgroup id="test" paddingLeft="10" fontSize="12" verticalAlignment="top">
+                <cspan fontSize="26" text={formatValueToUnit(item.temperature, UNITS.Celcius)} />
+                <cspan color={textLightColor} text={item.temperature !== item.apparentTemperature ? ' ' + formatValueToUnit(item.apparentTemperature, UNITS.Celcius) : null} />
+            </cgroup>
+        {:else}
+            <cgroup paddingLeft="10" fontSize="12" verticalAlignment="top">
+                <cspan fontSize="26" text={formatValueToUnit(item.temperatureMin, UNITS.Celcius)} />
+                <cspan color="#777" fontSize="26" text=" | " />
+                <cspan fontSize="26" text={formatValueToUnit(item.temperatureMax, UNITS.Celcius)} />
+            </cgroup>
+        {/if}
 
-    <wraplayout row="1" verticalAlignment="top" horizontalAlignment="left">
-        <label
-            width="60"
-            fontSize="14"
-            horizontalAlignment="left"
-            verticalAlignment="top"
-            textAlignment="center"
-            paddingTop="10"
-            html={`<span style=" font-size:24px; font-family:${wiFontFamily};">${item.windIcon}</span><br>${formatValueToUnit(item.windSpeed, UNITS.Speed)}`}>
-            <!-- <span fontSize="26" fontFamily={wiFontFamily} text={item.windIcon + '\n'} />
-            <span text={formatValueToUnit(item.windSpeed, UNITS.Speed)} /> -->
-        </label>
-        <label
-            width="60"
-            fontSize="14"
-            color={item.cloudColor}
-            visibility={item.cloudCover > 0 ? 'visible' : 'collapsed'}
-            horizontalAlignment="left"
-            verticalAlignment="top"
-            textAlignment="center"
-            paddingTop="10"
-            html={`<span style="font-size:24px; font-family:${wiFontFamily};">wi-cloud</span><br>${Math.round(item.cloudCover * 100)}%${item.cloudCeiling ? `<br><span style="font-size:6;">${formatValueToUnit(item.cloudCeiling, UNITS.Distance)}</span>` : ''}`} />
-        <label
-            width="60"
-            fontSize="14"
-            visibility={item.precipIntensity >= 0.1 && item.precipProbability > 0.1 ? 'visible' : 'collapsed'}
-            color={rainColor}
-            horizontalAlignment="left"
-            verticalAlignment="top"
-            textAlignment="center"
-            paddingTop="10"
-            html={`<span style=" font-size:24px; font-family:${wiFontFamily};">wi-raindrop</span><br>${item.precipIntensity >= 0.1 ? formatValueToUnit(item.precipIntensity, UNITS.MM) + '<br>' : ''}${Math.round(item.precipProbability * 100)}%`}>
-            <!-- <span fontSize="26" fontFamily={wiFontFamily} text={'wi-raindrop' + '\n'} />
-            <span text={Math.round(item.precipProbability * 100) + '%'} /> -->
-        </label>
-        <label
-            width="60"
-            fontSize="14"
-            color={nightColor}
-            horizontalAlignment="left"
-            verticalAlignment="top"
-            textAlignment="center"
-            paddingTop="10"
-            html={`<span style=" font-size:24px; font-family:${wiFontFamily};">${item.moonIcon}</span><br>${l('moon')}`}>
-            <!-- <span fontSize="26" fontFamily={wiFontFamily} text={item.moonIcon + '\n'} /> -->
-        </label>
-    </wraplayout>
-    <!-- <label marginLeft="10" row="0" rowSpan="2" fontSize="14" html={textHtmlBottom} verticalTextAlignment="bottom" /> -->
+        <cgroup paddingLeft="0" paddingTop="40" fontSize="14" verticalAlignment="top" width="60" textAlignment="center">
+            <cspan fontSize="24" fontFamily={wiFontFamily} text={item.windIcon} />
+            <cspan text={'\n' + formatValueToUnit(item.windSpeed, UNITS.Speed)} />
+        </cgroup>
+        <cgroup paddingLeft="60" paddingTop="40" fontSize="14" verticalAlignment="top" width="60" textAlignment="center" color={nightColor}>
+            <cspan fontSize="24" fontFamily={wiFontFamily} text={item.moonIcon} />
+            <cspan text={'\n' + l('moon')} />
+        </cgroup>
+        {#if item.cloudCover > 0}
+            <cgroup paddingLeft="120" paddingTop="40" fontSize="14" verticalAlignment="top" width="60" textAlignment="center" color={item.cloudColor}>
+                <cspan fontSize="24" fontFamily={wiFontFamily} text="wi-cloud" />
+                <cspan text={'\n' + Math.round(item.cloudCover * 100) + '%'} />
+                <cspan fontSize="6" text={item.cloudCeiling ? '\n' + formatValueToUnit(item.cloudCeiling, UNITS.Distance) : null} />
+            </cgroup>
+        {/if}
+        {#if item.precipIntensity >= 0.1 && item.precipProbability > 0.1}
+            <cgroup color={rainColor} paddingLeft={item.cloudCover > 0 ? 180 : 120} paddingTop="40" fontSize="14" verticalAlignment="top" width="60" textAlignment="center">
+                <cspan fontSize="20" fontFamily={wiFontFamily} text="wi-raindrop" />
+                <cspan text={item.precipIntensity >= 0.1 ? formatValueToUnit(item.precipIntensity, UNITS.MM) : null} />
+                <cspan text={'\n' + Math.round(item.precipProbability * 100) + '%'} />
+            </cgroup>
+        {/if}
 
-    <linechart bind:this={lineChart} row="2" verticalAlignment="bottom" height="90" />
-    <WeatherIcon row="1" col="1" rowSpan="2" horizontalAlignment="right" verticalAlignment="center" fontSize="140" icon={item.icon} />
+        <cgroup paddingLeft="10" paddingBottom="10" fontSize="14" verticalAlignment="bottom">
+            <cspan color="#ffa500" fontFamily={wiFontFamily} text="wi-sunrise" />
+            <cspan text={convertTime(item.sunriseTime, 'HH:mm')} />
+            <cspan color="#ff7200" fontFamily={wiFontFamily} text="wi-sunset" />
+            <cspan text={convertTime(item.sunsetTime, 'HH:mm')} />
+        </cgroup>
+        <cspan paddingRight="10" fontSize="14" textAlignment="right" verticalAlignment="bottom" text="{l('last_updated')}: {formatLastUpdate(item.lastUpdate)}" paddingBottom="10" />
+    </canvaslabel>
 
-    <label
-        id="testSpan"
-        marginLeft="10"
-        marginTop="20"
-        fontSize="14"
-        row="3"
-        verticalTextAlignment="bottom"
-        html={`<font face="${wiFontFamily}" color="#ffa500">wi-sunrise</font>${convertTime(item.sunriseTime, 'HH:mm')}<font face="${wiFontFamily}" color="#ff7200">wi-sunset</font>${convertTime(item.sunsetTime, 'HH:mm')}`}  marginBottom="10">
-
-        <!-- <span fontFamily={wiFontFamily} fontSize="16" text="wi-sunrise" color="#ffa500" />
-        <span text=" {convertTime(item.sunriseTime, 'HH:mm')} " />
-        <span fontFamily={wiFontFamily} fontSize="16" text="wi-sunset" color="#ff7200" />
-        <span text=" {convertTime(item.sunsetTime, 'HH:mm')}" /> -->
-    </label>
-    <label marginRight="10" row="3" col="0" colSpan="2" fontSize="14" textAlignment="right" verticalTextAlignment="bottom" text="{l('last_updated')}: {formatLastUpdate(item.lastUpdate)}"  marginBottom="10" />
-    <HourlyView row="4" colSpan="2"items={item.hourly} />
+    <linechart bind:this={lineChart} marginTop="110" verticalAlignment="bottom" height="90"  marginBottom="20"/>
+    <WeatherIcon col="1" horizontalAlignment="right" verticalAlignment="center" fontSize="140" icon={item.icon} />
+    <HourlyView row="1" colSpan="2" items={item.hourly} />
 </gridLayout>

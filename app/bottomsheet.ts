cb5db9fdbb4c70bf356ddf8e7fc583d74a14c590
Martin Guillon <martin.guillon@akylas.fr>
2021-02-24 16:17:53 +0100

    some fixes

--- app/bottomsheet.ts
+++ app/bottomsheet.ts
@@ -1,5 +1,6 @@
 import { BottomSheetOptions } from '@nativescript-community/ui-material-bottomsheet';
-import { Frame, View, ViewBase } from '@nativescript/core';
+import { View, ViewBase } from '@nativescript/core';
+import { Frame } from '@nativescript/core/ui/frame';
 import { NativeViewElementNode, createElement } from 'svelte-native/dom';
 import { PageSpec } from 'svelte-native/dom/navigation';
 
@@ -15,12 +16,13 @@ interface ComponentInstanceInfo {
 
 const modalStack: ComponentInstanceInfo[] = [];
 
-function resolveComponentElement(viewSpec: PageSpec, props?: any): ComponentInstanceInfo {
+export function resolveComponentElement(viewSpec: PageSpec, props?: any): ComponentInstanceInfo {
     const dummy = createElement('fragment');
     const viewInstance = new viewSpec({ target: dummy, props });
     const element = dummy.firstElement() as NativeViewElementNode<View>;
     return { element, viewInstance };
 }
+
 export function showBottomSheet<T>(modalOptions: ShowBottomSheetOptions): Promise<T> {
     const { view, parent, props = {}, ...options } = modalOptions;
     // Get this before any potential new frames are created by component below
@@ -29,10 +31,11 @@ export function showBottomSheet<T>(modalOptions: ShowBottomSheetOptions): Promis
     const componentInstanceInfo = resolveComponentElement(view, props);
     const modalView: ViewBase = componentInstanceInfo.element.nativeView;
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         let resolved = false;
         const closeCallback = (result: T) => {
             if (resolved) return;
+            modalStack.pop();
             resolved = true;
             resolve(result);
             componentInstanceInfo.viewInstance.$destroy(); // don't let an exception in destroy kill the promise callback
@@ -47,8 +50,11 @@ export function showBottomSheet<T>(modalOptions: ShowBottomSheetOptions): Promis
 }
 
 export function closeBottomSheet(result?: any): void {
-    const modalPageInstanceInfo = modalStack.pop();
+    const modalPageInstanceInfo = modalStack[modalStack.length - 1];
     if (modalPageInstanceInfo) {
         (modalPageInstanceInfo.element.nativeView as any).closeBottomSheet(result);
     }
+}
+export function isBottomSheetOpened() {
+    return modalStack.length > 0;
 }

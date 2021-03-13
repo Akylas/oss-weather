b40e3ec17082bab5e095dabbd633223fff5c7332
Martin Guillon <martin.guillon@akylas.fr>
2021-03-13 21:42:22 +0100

    fix: bunch of fixes

--- app/bottomsheet.ts
+++ app/bottomsheet.ts
@@ -1,6 +1,6 @@
 import { BottomSheetOptions } from '@nativescript-community/ui-material-bottomsheet';
 import { View, ViewBase } from '@nativescript/core';
-import { Frame } from '@nativescript/core/ui/frame';
+import { Frame } from '@nativescript/core';
 import { NativeViewElementNode, createElement } from 'svelte-native/dom';
 import { PageSpec } from 'svelte-native/dom/navigation';
 

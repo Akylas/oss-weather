ef1183b8ec2846c340d2fc934a3a98472c39eb69
Martin Guillon <martin.guillon@akylas.fr>
2021-05-07 17:35:33 +0200

    chore:we bpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,46 @@
-const webpack = require("@nativescript/webpack");
+const webpack = require('@nativescript/webpack');
 module.exports = (env) => {
     webpack.init(env);
-    // todo: comments for common usage
+    webpack.useConfig('svelte');
+    // We want to use svelte-loader for now
+    webpack.chainWebpack((config, env) => {
+        // target('node') is the default and causes svelte-loader to detect it as a "server" render, disabling HMR
+        config.target('electron-main');
+
+        // svelte-hmr references tns-core-modules, so we shim it here for now
+        config.resolve.alias.set('tns-core-modules', '@nativescript/core');
+
+        // the default svelte config uses deprecated svelte-loader-hot, we replace it here while keeping the preprocessor config.
+        let opts = null;
+        config.module
+            .rule('svelte')
+            .uses.get('svelte-loader-hot')
+            .tap((opt) => (opts = opt));
+
+        config.module.rule('svelte').clear();
+        config.module.rule('svelte').uses.clear();
+
+        const production = env.production === 'production';
+        config.module
+            .rule('svelte')
+            .test(/\.svelte$/)
+            .exclude.add(/node_modules/)
+            .end()
+            .use('svelte-loader')
+            .loader('svelte-loader')
+            .tap((opt) => ({
+                compilerOptions: {
+                    dev: !production,
+                    namespace: 'foreign'
+                },
+                preprocess: opts.preprocess,
+                hotReload: !production,
+                hotOptions: {
+                    injectCss: false,
+                    native: true
+                }
+            }));
+        return config;
+    });
     return webpack.resolveConfig();
 };
-//# sourceMappingURL=default.config.stub.js.map

4406b8fe2c9c36681293600b32f4a3cbabfc94bc
Martin Guillon <martin.guillon@akylas.fr>
2021-07-12 16:32:05 +0200

    fix: weather warning color change

--- app/AlertView.svelte
+++ app/AlertView.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    import { convertTime,titlecase } from '~/helpers/formatter';
+    import { convertTime, titlecase } from '~/helpers/formatter';
     import { l } from '~/helpers/locale';
     import { Alert } from '~/services/api';
-
 
     export let alerts: Alert[];
 </script>
@@ -11,15 +10,15 @@
     <stackLayout>
         {#each alerts as alert}
             <gridLayout class="alertView" columns="auto,*" rows="auto">
-                <label verticalAlignment="top" marginLeft="10" color="#ff4f3c" fontSize="36" class="icon-btn" text="mdi-alert" />
-                <label col="1" fontSize="14" marginLeft="4" verticalAlignment="top" textWrap={true}>
+                <label verticalAlignment="top" marginLeft="10" color="#EFB644" fontSize="36" class="icon-btn" text="mdi-alert" />
+                <label col="1" fontSize="14" padding="0 4 4 0" verticalAlignment="top" textWrap={true}>
                     <formattedString>
                         <span fontSize="17" text="{alert.event}{'\n'}" />
-                        <span fontSize="17" text="{alert.sender_name}{'\n'}" />
-                        <span text="{titlecase(l('expires'))}: {convertTime(alert.end, 'HH:mm dddd')}{'\n'}" />
-                        <span color="#aaa" text={alert.description} />
+                    <span fontSize="17" text="{alert.sender_name}{'\n'}" />
+                    <span text="{titlecase(l('expires'))}: {convertTime(alert.end, 'HH:mm dddd')}{'\n'}" />
+                    <span color="#aaa" text={alert.description} />
                     </formattedString>
-                </label>
+                    </label>
             </gridLayout>
         {/each}
     </stackLayout>

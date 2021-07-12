b11ed4b2905156706072331f96c02485f32cda79
Martin Guillon <martin.guillon@akylas.fr>
2021-07-12 16:31:19 +0200

    fix: show shadows to advertise scroll

--- app/HourlyView.svelte
+++ app/HourlyView.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Template } from 'svelte-native/components';
+    import { backgroundColor } from './variables';
     import WeatherCollectionItem from './WeatherCollectionItem.svelte';
 
     export let items;
@@ -9,23 +10,39 @@
     function onDataPopulated() {
         collectionView.nativeView.scrollToIndex(scrollIndex, false);
     }
+
+    let showLeftShadow = false;
+    let showRightShadow = true;
+    function onScrollEvent(event) {
+        showLeftShadow = event.scrollOffsetPercentage > 0;
+        showRightShadow = event.scrollOffsetPercentage < 1;
+    }
 </script>
 
-<collectionview
-    {...$$restProps}
-    disableCss={true}
-    bind:this={collectionView}
-    itemIdGenerator={(_item, index) => index}
-    orientation="horizontal"
-    colWidth="60"
-    rowHeight="100%"
-    height="100%"
-    isBounceEnabled="false"
-    iosOverflowSafeAreaEnabled="false"
-    {items}
-    on:dataPopulated={onDataPopulated}
->
-    <Template let:item>
-        <WeatherCollectionItem {item} />
-    </Template>
-</collectionview>
+<gridlayout {...$$restProps}>
+    <collectionview
+        bind:this={collectionView}
+        itemIdGenerator={(_item, index) => index}
+        orientation="horizontal"
+        colWidth="61"
+        rowHeight="100%"
+        height="100%"
+        isBounceEnabled="false"
+        iosOverflowSafeAreaEnabled="false"
+        {items}
+        on:dataPopulated={onDataPopulated}
+        on:scroll={onScrollEvent}
+    >
+        <Template let:item>
+            <WeatherCollectionItem {item} />
+        </Template>
+    </collectionview>
+    <absolutelayout visibility={showLeftShadow ? 'visible' : 'hidden'} background={`linear-gradient(to right, ${$backgroundColor}, transparent)`} height="100%" width={40} horizontalAlignment="left" />
+    <absolutelayout
+        visibility={showRightShadow ? 'visible' : 'hidden'}
+        background={`linear-gradient(to right, transparent, ${$backgroundColor})`}
+        height="100%"
+        width={40}
+        horizontalAlignment="right"
+    />
+</gridlayout>

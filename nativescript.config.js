7df4f28fa4220bd7dea4341a27de2f5c2c480cb7
Martin Guillon <martin.guillon@akylas.fr>
2021-10-04 11:09:49 +0200

    chore: ignore sentry we dont use it

--- nativescript.config.js
+++ nativescript.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     id: 'com.akylas.weather',
+    ignoredNativeDependencies: ['@nativescript-community/sentry'],
     appResourcesPath: 'App_Resources',
     webpackConfigPath: './app.webpack.config.js',
     appPath: 'app',
+    forceLog: true,
     // profiling: 'timeline',
     android: {
         maxLogcatObjectSize: 4096,
         markingMode: 'none',
-        v8Flags: '--expose_gc',
+        // v8Flags: '--expose_gc',
         codeCache: true,
         enableMultithreadedJavascript: false,
         forceLog: true

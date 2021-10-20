684d9ce67a95b2aef131a1da626e8ce3e2d6dce0
Martin Guillon <martin.guillon@akylas.fr>
2021-10-20 16:47:06 +0200

    chore: optim for third parties list

--- app/ThirdPartySoftwareBottomSheet.svelte
+++ app/ThirdPartySoftwareBottomSheet.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Template } from 'svelte-native/components';
     import { openLink } from '~/utils/ui';
-    import SettingLabelIcon from './SettingLabelIcon.svelte';
+    import { primaryColor } from './variables';
 
     let licences = require(`~/licenses.json`);
     let items = licences.dependencies;
@@ -12,8 +12,11 @@
     }
 </script>
 
-<collectionView id="trackingScrollView" {items} rowHeight="60" itemIdGenerator={(item, i) => i} class="bottomsheet" height="300">
+<collectionView id="trackingScrollView" {items} rowHeight={60} itemIdGenerator={(item, i) => i} class="bottomsheet" height={300}>
     <Template let:item>
-        <SettingLabelIcon title={item.moduleName} subtitle={item.moduleUrl} on:tap={() => onTap(item)} />
+        <canvaslabel padding="12 16 12 16" rippleColor={primaryColor} on:tap={() => onTap(item)}>
+            <cspan text={item.moduleName} verticalAlignment="top" fontSize={17} />
+            <cspan text={item.moduleUrl} color="#aaaaaa" verticalAlignment="bottom" fontSize={14} />
+        </canvaslabel>
     </Template>
 </collectionView>

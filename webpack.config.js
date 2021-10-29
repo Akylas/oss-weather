cb194ba42fe66f3d194411ddafe81454a1605ad6
Martin Guillon <martin.guillon@akylas.fr>
2021-10-29 14:46:04 +0200

    chore: svelte build process

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const webpack = require('@nativescript/webpack');
 module.exports = (env) => {
     webpack.init(env);
     webpack.chainWebpack((config, env) => {
+        // turns out this isn't enough now. svelte uses "node" of which "electron-main" is a subset in its export map forcing imports
+        // for 'svelte' to 'ssr.mjs'. We define an alias here to force it back.
+        config.resolve.alias.set('svelte$', 'svelte/internal');
+
         config.module
             .rule('svelte')
             .use('string-replace-loader')

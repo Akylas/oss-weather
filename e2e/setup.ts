2ad146fc7aa8ef9ab531b70715ae67585aaac930
Martin Guillon <martin.guillon@akylas.fr>
2021-10-31 14:46:06 +0100

    chore: detox

--- e2e/setup.ts
+++ e2e/setup.ts
@@ -1,20 +1,20 @@
-import { cleanup, init } from "detox";
-const adapter = require('detox/runners/jest/adapter')
+import { cleanup, init } from 'detox';
+const adapter = require('detox/runners/jest/adapter');
 
-const config = require("../package.json").detox;
+const config = require('../package.json').detox;
 
 jest.setTimeout(120000);
 (jasmine as any).getEnv().addReporter(adapter);
 
 beforeAll(async () => {
-  await init(config, { initGlobals: false });
+    await init(config, { initGlobals: false });
 });
 
 beforeEach(async () => {
-  await adapter.beforeEach();
+    await adapter.beforeEach();
 });
 
 afterAll(async () => {
-  await adapter.afterAll();
-  await cleanup();
+    await adapter.afterAll();
+    await cleanup();
 });
--- e2e/tests/screenshots.spec.ts
+++ e2e/tests/screenshots.spec.ts
@@ -1,31 +1,29 @@
 import { by, device, element, expect, waitFor } from 'detox';
 import { copyFile } from 'fs';
-import { join, dirname , basename} from 'path';
+import { basename, dirname, join } from 'path';
 
+const platform = gVars.platform;
 describe('Screenshots', () => {
     beforeEach(async () => {
         await device.reloadReactNative();
     });
 
-
     it('scree', async () => {
-        await saveScreenshot('screen1');  
+        await saveScreenshot('screen1');
         await element(by.label('skip')).tap();
-        await waitFor(element(by.label('skip'))).toBeNotVisible();
-        await saveScreenshot('screen2');  
-
-    });  
+        await expect(element(by.label('skip'))).not.toBeVisible();
+        await saveScreenshot('screen2');
+    });
 });
-async function saveScreenshot(screenshotName) {
-    const imagePath = await device.takeScreenshot(screenshotName) as any;
-    return new Promise(async (resolve,reject)=>{
-        copyFile(imagePath, `./fastlane/screenshots/${screenshotName}.png`, (err)=>{
+async function saveScreenshot(lang, screenshotName) {
+    const imagePath = (await device.takeScreenshot(screenshotName)) as any;
+    return new Promise<void>(async (resolve, reject) => {
+        copyFile(imagePath, `./fastlane/metadata/${platform}/${lang}/screenshots/${screenshotName}.png`, (err) => {
             if (err) {
-                reject(err)
+                reject(err);
             } else {
                 resolve();
             }
         });
-    })
-    
-  }
+    });
+}

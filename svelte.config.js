30e0506df06892fe69469365d902ca0d2e7c22b3
Martin Guillon <martin.guillon@akylas.fr>
2022-02-15 09:15:45 +0100

    chore: svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,26 @@
 const sveltePreprocess = require('svelte-preprocess');
-const svelteNativePreprocessor = require('svelte-native-preprocessor');
+// const svelteNativePreprocessor = require('svelte-native-preprocessor');
+// const { transformSync } = require('@swc/core');
+// const { typescript } = require('svelte-preprocess-esbuild');
 
 module.exports = {
     compilerOptions: {
-        namespace: 'foreign',
+        namespace: 'foreign'
     },
     preprocess: [
+        // typescript({
+        //     target: 'es2019'
+        // }),
+        // sveltePreprocess({
+        //     typescript: false
+        // })
         sveltePreprocess({
             typescript: {
                 compilerOptions: {
-                    target: 'es2017'
+                    target: 'es2019'
                 }
             }
-        }),
-        svelteNativePreprocessor()
+        })
+        // svelteNativePreprocessor()
     ]
 };

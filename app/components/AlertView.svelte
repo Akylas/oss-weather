467c3ddb9e9489e6389baebdd36813a366887976
Martin Guillon <martin.guillon@akylas.fr>
2022-02-27 16:15:15 +0100

    fix: weather alert fix

--- app/components/AlertView.svelte
+++ app/components/AlertView.svelte
@@ -12,12 +12,10 @@
             <gridLayout class="alertView" columns="auto,*" rows="auto">
                 <label verticalAlignment="top" marginLeft={10} color="#EFB644" fontSize={36} class="icon-btn" text="mdi-alert" />
                 <label col={1} fontSize={14} padding="0 4 4 0" verticalAlignment="top" textWrap={true}>
-                    <formattedString>
-                        <span fontSize={17} text="{alert.event}{'\n'}" />
-                        <span fontSize={17} text="{alert.sender_name}{'\n'}" />
-                        <span text="{titlecase(l('expires'))}: {convertTime(alert.end, 'HH:mm dddd')}{'\n'}" />
-                        <span color="#aaa" text={alert.description} />
-                    </formattedString>
+                    <span fontSize={17} text="{alert.event}{'\n'}" />
+                    <span fontSize={17} text="{alert.sender_name}{'\n'}" />
+                    <span text="{titlecase(l('expires'))}: {convertTime(alert.end, 'HH:mm dddd')}{'\n'}" />
+                    <span color="#aaa" text={alert.description} />
                 </label>
             </gridLayout>
         {/each}

c2af8ee20286f7fc2312b9748a825cf15dc6fe4d
Martin Guillon <martin.guillon@akylas.fr>
2022-03-01 17:16:29 +0100

    chore: lint

--- app/transitions/custom-transition.android.ts
+++ app/transitions/custom-transition.android.ts
@@ -1,12 +1,8 @@
-import {
-    Transition
-} from '@nativescript/core/ui/transition';
+import { Transition } from '@nativescript/core/ui/transition';
 export class CustomTransition extends Transition {
-    public createAndroidAnimator(
-        transitionType: string
-    ): android.animation.Animator {
-        const scaleValues = (Array).create('float', 2);
-        const opacityValues = (Array).create('float', 2);
+    public createAndroidAnimator(transitionType: string): android.animation.Animator {
+        const scaleValues = Array.create('float', 2);
+        const opacityValues = Array.create('float', 2);
         switch (transitionType) {
             case Transition.AndroidTransitionType.enter:
                 scaleValues[0] = 0.5;
@@ -33,22 +29,10 @@ export class CustomTransition extends Transition {
                 opacityValues[1] = 0;
                 break;
         }
-        const objectAnimators = (Array).create(android.animation.Animator, 3);
-        objectAnimators[0] = android.animation.ObjectAnimator.ofFloat(
-            null,
-            'scaleX',
-            scaleValues
-        );
-        objectAnimators[1] = android.animation.ObjectAnimator.ofFloat(
-            null,
-            'scaleY',
-            scaleValues
-        );
-        objectAnimators[2] = android.animation.ObjectAnimator.ofFloat(
-            null,
-            'alpha',
-            opacityValues
-        );
+        const objectAnimators = Array.create(android.animation.Animator, 3);
+        objectAnimators[0] = android.animation.ObjectAnimator.ofFloat(null, 'scaleX', scaleValues);
+        objectAnimators[1] = android.animation.ObjectAnimator.ofFloat(null, 'scaleY', scaleValues);
+        objectAnimators[2] = android.animation.ObjectAnimator.ofFloat(null, 'alpha', opacityValues);
 
         const animatorSet = new android.animation.AnimatorSet();
         animatorSet.playTogether(objectAnimators);

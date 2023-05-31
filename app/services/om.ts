467c3a88dbe81fa071df95a6da9667b56e382a40
farfromrefuge <dev@akylas.fr>
2023-05-31 10:41:23 +0200

    fix: open-meteo fixes (preparing custom model)

--- app/services/om.ts
+++ app/services/om.ts
@@ -6,6 +6,7 @@ import { l, lang } from '~/helpers/locale';
 import { WeatherLocation, request } from './api';
 import { Forecast } from './openmeteo';
 import { GetTimesResult, getTimes } from 'suncalc';
+import { ApplicationSettings } from '@nativescript/core';
 // import { Coord, Dailyforecast, Forecast, MFCurrent, MFForecastResult, MFMinutely, MFWarnings, Probabilityforecast } from './meteofrance';
 
 // const mfApiKey = getString('mfApiKey', MF_DEFAULT_KEY);
@@ -19,11 +20,11 @@ export interface OMParams {
     longitude?: number;
 }
 
-function round5(t) {
-    return 5 * Math.ceil(t / 5);
-}
-const SixHours = 6 * 3600;
-const ThirtyHours = 30 * 3600;
+// function round5(t) {
+//     return 5 * Math.ceil(t / 5);
+// }
+// const SixHours = 6 * 3600;
+// const ThirtyHours = 30 * 3600;
 // function getDaily(weatherLocation: WeatherLocation, hourly: Hourly[], hourlyForecast: Forecast[], dailyForecast: Dailyforecast) {
 //     let precipitationTotal = 0;
 //     const probPrecipitationTotal = { count: 0, total: 0 };
@@ -180,7 +181,7 @@ const weatherCodeDescription = {
 };
 
 function convertWeatherCodeToIcon(code: number, is_day: number | boolean = 1) {
-    const actualCode = code % 100;
+    // const actualCode = code % 100;
     const night = is_day ? 'd' : 'n';
     switch (code) {
         case 0:
@@ -223,12 +224,19 @@ function convertWeatherCodeToIcon(code: number, is_day: number | boolean = 1) {
 }
 
 // https://api.open-meteo.com/v1/forecast?latitude=45.18&longitude=5.71&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,windspeed_10m,winddirection_10m,windgusts_10m,uv_index,uv_index_clear_sky&models=best_match&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant&timeformat=unixtime&forecast_days=14&timezone=auto
-export async function fetch<T = any>(apiName: string = 'forecast', queryParams: OMParams = {}) {
+export async function fetch<T = any>(apiName: string = 'forecast', queryParams: OMParams = {}, preferedModel?) {
+    let models = 'best_match';
+    if (preferedModel) {
+        models += ',' + preferedModel;
+    }
+    console.log('models', models);
     return request<T>({
         url: `https://api.open-meteo.com/v1/${apiName}`,
         method: 'GET',
         queryParams: {
             hourly: 'temperature_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weathercode,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,windspeed_10m,winddirection_10m,windgusts_10m,uv_index,uv_index_clear_sky,is_day,freezinglevel_height,snow_depth',
+            models,
+
             // models: 'best_match',
             // models: 'meteofrance_seamless',
             daily: 'weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant',
@@ -240,53 +248,68 @@ export async function fetch<T = any>(apiName: string = 'forecast', queryParams: 
         }
     });
 }
-
+function getDataArray(object: any, key: string, model?: string) {
+    if (model) {
+        return object[key + '_' + model] || object[key + '_best_match'];
+    } else {
+        return object[key];
+    }
+}
 export async function getWeather(weatherLocation: WeatherLocation) {
     const coords = weatherLocation.coord;
-    const forecast = await fetch<Forecast>('forecast', { latitude: coords.lat, longitude: coords.lon });
-    // console.log('forecast', JSON.stringify(forecast.forecast));
+    const preferedModel = ApplicationSettings.getString('open_meteo_prefered_model');
+    const forecast = await fetch<Forecast>('forecast', { latitude: coords.lat, longitude: coords.lon }, preferedModel);
+    // console.log('forecast', Object.keys(forecast), Object.keys(forecast.hourly));
     // console.log('rain', JSON.stringify(rain));
     // console.log('current', JSON.stringify(current));
     // console.log('warnings', JSON.stringify(warnings));
     const hourly = forecast.hourly;
-    let hourlyLastIndex = hourly.weathercode.findIndex((d) => d === null);
+    const hourly_weathercodes = getDataArray(hourly, 'weathercode', preferedModel);
+    let hourlyLastIndex = hourly_weathercodes.findIndex((d) => d === null);
     if (hourlyLastIndex === -1) {
-        hourlyLastIndex = hourly.weathercode.length -1;
+        hourlyLastIndex = hourly_weathercodes.length - 1;
     }
     const hourlyData = hourly.time.slice(0, hourlyLastIndex).map((time, index) => {
         const d = {} as Hourly;
         d.time = time * 1000;
-        const code = hourly.weathercode[index];
-        d.icon = convertWeatherCodeToIcon(code, hourly.is_day[index]);
+        const code = hourly_weathercodes[index];
+        d.icon = convertWeatherCodeToIcon(code, getDataArray(hourly, 'is_day', preferedModel)[index]);
         d.description = weatherCodeDescription[code];
-        d.temperature = Math.round(hourly.temperature_2m[index]);
-        d.feelTemperature = Math.round(hourly.apparent_temperature[index]);
+        d.temperature = Math.round(getDataArray(hourly, 'temperature_2m', preferedModel)[index]);
+        d.feelTemperature = Math.round(getDataArray(hourly, 'apparent_temperature', preferedModel)[index]);
 
-        d.windBearing = hourly.winddirection_10m[index];
+        d.windBearing = getDataArray(hourly, 'winddirection_10m', preferedModel)[index];
 
         const hasNext = index < hourlyLastIndex;
 
-        if (hourly.precipitation_probability && hasNext) {
-            d.precipProbability = hourly.precipitation_probability[index + 1] || -1;
+        const precipitation_probability = getDataArray(hourly, 'precipitation_probability', preferedModel);
+        if (precipitation_probability && hasNext) {
+            d.precipProbability = precipitation_probability[index + 1] || -1;
         }
-        if (hasNext && hourly.snowfall) {
-            d.snowfall = hourly.snowfall[index + 1] || -1;
-        }
-
-        if (hasNext && hourly.precipitation) {
-            d.precipAccumulation = hourly.precipitation[index + 1] || -1;
+        const snowfall = getDataArray(hourly, 'snowfall', preferedModel);
+        if (hasNext && snowfall) {
+            d.snowfall = snowfall[index + 1] || -1;
         }
 
-        d.cloudCover = hourly.cloudcover[index];
-        if (hasNext && hourly.windgusts_10m) {
-            d.windGust = hourly.windgusts_10m[index + 1];
+        const precipitation = getDataArray(hourly, 'precipitation', preferedModel);
+        if (hasNext && precipitation) {
+            d.precipAccumulation = precipitation[index + 1] || -1;
         }
-        d.windSpeed = hourly.windspeed_10m[index];
-        if (hourly.snow_depth) {
-            d.snowDepth = hourly.snow_depth[index];
+
+        d.cloudCover = getDataArray(hourly, 'cloudcover', preferedModel)[index];
+        d.windSpeed = getDataArray(hourly, 'windspeed_10m', preferedModel)[index];
+
+        const windgusts_10m = getDataArray(hourly, 'windgusts_10m', preferedModel);
+        if (hasNext && windgusts_10m) {
+            d.windGust = windgusts_10m[index + 1];
         }
-        if (hourly.freezinglevel_height) {
-            d.iso = hourly.freezinglevel_height[index];
+        const snow_depth = getDataArray(hourly, 'snow_depth', preferedModel);
+        if (snow_depth) {
+            d.snowDepth = snow_depth[index];
+        }
+        const freezinglevel_height = getDataArray(hourly, 'freezinglevel_height', preferedModel);
+        if (freezinglevel_height) {
+            d.iso = freezinglevel_height[index];
         }
         // if (typeof data['rain snow limit'] === 'number') {
         //     d.rainSnowLimit = data['rain snow limit'];
@@ -296,7 +319,8 @@ export async function getWeather(weatherLocation: WeatherLocation) {
     });
     const current = forecast.current_weather;
     const daily = forecast.daily;
-    const dailyLastIndex = daily.weathercode.findIndex((d) => d === null);
+    const daily_weathercodes = getDataArray(daily, 'weathercode', preferedModel);
+    const dailyLastIndex = daily_weathercodes.findIndex((d) => d === null);
     const r = {
         currently: current
             ? weatherDataIconColors(
@@ -314,20 +338,20 @@ export async function getWeather(weatherLocation: WeatherLocation) {
             : {},
         daily: {
             data: daily.time.slice(0, dailyLastIndex).map((time, index) => {
-                const code = daily.weathercode[index];
+                const code = daily_weathercodes[index];
                 const d = {
                     time: time * 1000,
                     description: weatherCodeDescription[code],
                     icon: convertWeatherCodeToIcon(code, true),
-                    temperatureMax: daily.temperature_2m_max[index],
-                    temperatureMin: daily.temperature_2m_min[index],
+                    temperatureMax: getDataArray(daily, 'temperature_2m_max', preferedModel)[index],
+                    temperatureMin: getDataArray(daily, 'temperature_2m_min', preferedModel)[index],
                     // humidity: (dailyForecast.humidity.max + dailyForecast.humidity.min) / 2,
-                    uvIndex: Math.ceil(daily.uv_index_max?.[index]),
-                    windGust: Math.round(daily.windgusts_10m_max[index]),
-                    windSpeed: Math.round(daily.windspeed_10m_max[index]),
-                    windBearing: Math.round(daily.winddirection_10m_dominant[index]),
-                    precipProbability: daily.precipitation_probability_max?.[index],
-                    precipAccumulation: daily.precipitation_sum[index]
+                    uvIndex: Math.ceil(getDataArray(daily, 'uv_index_max', preferedModel)?.[index]),
+                    windGust: Math.round(getDataArray(daily, 'windgusts_10m_max', preferedModel)[index]),
+                    windSpeed: Math.round(getDataArray(daily, 'windspeed_10m_max', preferedModel)[index]),
+                    windBearing: Math.round(getDataArray(daily, 'winddirection_10m_dominant', preferedModel)[index]),
+                    precipProbability: getDataArray(daily, 'precipitation_probability_max', preferedModel)?.[index],
+                    precipAccumulation: getDataArray(daily, 'precipitation_sum', preferedModel)[index]
                     // cloudCover: Math.round(daily.clou),
                     // sunriseTime: dailyForecast.sun.rise * 1000,
                     // sunsetTime: dailyForecast.sun.set * 1000

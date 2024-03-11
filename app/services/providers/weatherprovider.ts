0d9e6a6732bd38139b2f308b953fb5b368fe916b
farfromrefug <dev@akylas.fr>
2024-03-11 15:22:40 +0100

    chore: refactor

--- app/services/providers/weatherprovider.ts
+++ app/services/providers/weatherprovider.ts
@@ -1,18 +1,28 @@
 import { WeatherLocation } from '../api';
-import { ProviderType } from './weather';
+import { ProviderType, WeatherData } from './weather';
+import { lang, lc } from '~/helpers/locale';
+
+const singletons = {};
 
 export abstract class WeatherProvider {
+    id: string;
     private static _singleton: WeatherProvider;
-    private static _singletonProviderType: ProviderType;
+    // private static _singletonProviderType: ProviderType;
 
-    abstract getWeather(weatherLocation: WeatherLocation);
+    abstract getWeather(weatherLocation: WeatherLocation, model?: string): Promise<WeatherData>;
 
-    public static setInstance(newSingleton: WeatherProvider, singletonType: ProviderType): void {
-        WeatherProvider._singleton = newSingleton;
-        WeatherProvider._singletonProviderType = singletonType;
+    getModels() {
+        return {};
+    }
+    getName() {
+        return lc(`provider.${this.id}`);
     }
 
-    public static getInstance(): [WeatherProvider, ProviderType] {
-        return [WeatherProvider._singleton, WeatherProvider._singletonProviderType];
+    public static getInstance<T extends WeatherProvider>(this: new () => T) {
+        const key = this.prototype.constructor.name;
+        if (!singletons[key]) {
+            singletons[key] = new this();
+        }
+        return singletons[key];
     }
 }
--- app/services/providers/weatherproviderfactory.ts
+++ app/services/providers/weatherproviderfactory.ts
@@ -4,38 +4,48 @@ import { OMProvider } from './om';
 import { OWMProvider } from './owm';
 import { ProviderType } from './weather';
 import { ApplicationSettings } from '@akylas/nativescript';
+import { prefs } from '../preferences';
 
 export const providers = ['meteofrance', 'openweathermap', 'openmeteo'] as const;
 
+let currentProvider: WeatherProvider;
+prefs.on('key:provider', () => {
+    setProvider(getProviderType());
+});
+
 export function getProvider(): WeatherProvider {
-    const requestedProviderType = getProviderType();
-    // eslint-disable-next-line prefer-const
-    let [provider, providerType] = WeatherProvider.getInstance();
-    if (requestedProviderType !== providerType) {
-        provider = setProvider(requestedProviderType);
+    DEV_LOG && console.log('getProvider', getProviderType(), !!currentProvider);
+    if (!currentProvider) {
+        setProvider(getProviderType());
     }
-    return provider;
+    // const requestedProviderType = getProviderType();
+    // eslint-disable-next-line prefer-const
+    // let [provider, providerType] = WeatherProvider.getInstance();
+    // if (requestedProviderType !== providerType) {
+    //     provider = setProvider(requestedProviderType);
+    // }
+    // return provider;
+    return currentProvider;
 }
 export function getProviderType(): ProviderType {
     const requestedProviderType: ProviderType = (ApplicationSettings.getString('provider', DEFAULT_PROVIDER) || DEFAULT_PROVIDER) as ProviderType;
     return requestedProviderType;
 }
 
-function setProvider(newType: ProviderType): WeatherProvider {
-    let provider: WeatherProvider;
+export function getProviderForType(newType: ProviderType): WeatherProvider {
     switch (newType) {
         case 'openweathermap':
-            provider = new OWMProvider();
-            break;
+            return OWMProvider.getInstance();
 
         case 'meteofrance':
-            provider = new MFProvider();
-            break;
+            return MFProvider.getInstance();
         case 'openmeteo':
         default:
-            provider = new OMProvider();
-            break;
+            return OMProvider.getInstance();
     }
-    WeatherProvider.setInstance(provider, newType);
-    return provider;
+}
+
+function setProvider(newType: ProviderType): WeatherProvider {
+    currentProvider = getProviderForType(newType);
+    return currentProvider;
 }

8119619dd2a9d285c17a7953f14c2fae392ebb0a
farfromrefug <dev@akylas.fr>
2024-03-25 08:28:48 +0100

    chore: project

--- typings/references.d.ts
+++ typings/references.d.ts
@@ -8,26 +8,6 @@
 /// <reference path="../node_modules/@nativescript/core/global-types.d.ts" />
 /// <reference path="../node_modules/@nativescript-community/ui-material-bottomsheet/bottomsheet.d.ts" />
 
-declare module 'svelte/internal' {
-    export function get_current_component();
-}
-
-declare namespace com {
-    export namespace akylas {
-        export namespace weather {
-            class Utils {
-                static applyDayNight(context: android.content.Context, applyDynamicColors: boolean);
-                static applyDynamicColors(context: android.content.Context);
-                static getDimensionFromInt(context: android.content.Context, intToGet);
-                static getColorFromInt(context: android.content.Context, intToGet);
-                static getColorFromName(context: android.content.Context, intToGet);
-                static restartApp(context: android.content.Context, activity: android.app.Activity);
-                static getSystemLocale(): java.util.Locale;
-            }
-        }
-    }
-}
-
 declare const SUPPORTED_LOCALES: string[];
 declare const DEFAULT_LOCALE: string;
 declare const DEFAULT_THEME: string;
@@ -60,69 +40,4 @@ interface LatLonKeys {
     lat: number;
     lon: number;
     altitude?: number;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-qualifier
-declare namespace svelteNative.JSX {
-    type Override<What, With> = Omit<What, keyof With> & With;
-    type ViewKeys = keyof TViewAttributes;
-    type TViewAugmentedAttributes = Override<
-        TViewAttributes,
-        {
-            disableCss?: boolean;
-            rippleColor?: string;
-            sharedTransitionTag?: string;
-            verticalAlignment?: string;
-            dynamicElevationOffset?: string | number;
-            elevation?: string | number;
-        }
-    >;
-    type ViewAndroidAttributes = {
-        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewIOSAttributes = {
-        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewAttributes = TViewAugmentedAttributes & ViewAndroidAttributes & ViewIOSAttributes;
-
-    interface ButtonAttributes {
-        variant?: string;
-        shape?: string;
-    }
-    interface ImageAttributes {
-        noCache?: boolean;
-        imageRotation?: number;
-        colorMatrix?: number[];
-        blurRadius?: number;
-        fadeDuration?: number;
-        'on:rotateAnimated'?: (args: EventData) => void;
-    }
-    interface SpanAttributes {
-        verticalAlignment?: string;
-        verticalTextAlignment?: string;
-    }
-    interface SliderAttributes {
-        stepSize?: number;
-        trackBackgroundColor?: string;
-    }
-    interface PageAttributes {
-        statusBarColor?: string;
-        screenOrientation?: string;
-        keepScreenAwake?: boolean;
-        screenBrightness?: number;
-        'on:closingModally'?: (args: ShownModallyData) => void;
-        // "on:shownModally"?: (args: ShownModallyData) => void;
-    }
-    interface LabelAttributes {
-        autoFontSize?: boolean;
-        verticalTextAlignment?: string;
-        maxLines?: number;
-        minFontSize?: number;
-        maxFontSize?: number;
-        lineBreak?: string;
-        html?: string;
-        selectable?: boolean;
-        onlinkTap?;
-        'on:linkTap'?;
-    }
 }

c29a8008eb5b948f1c2590517a58d53a76eb7685
farfromrefug <dev@akylas.fr>
2024-08-01 16:25:07 +0200

    chore: refactor fix

--- app/services/providers/weatherprovider.ts
+++ app/services/providers/weatherprovider.ts
@@ -8,5 +8,7 @@ export abstract class WeatherProvider extends Provider {
         weatherLocation: WeatherLocation,
         options?: { model?: string; warnings?: boolean; minutely?: boolean; hourly?: boolean; current?: boolean; forceModel?: boolean; weatherProps?: WeatherProps[] }
     ): Promise<WeatherData>;
-
+    getModels() {
+        return {};
+    }
 }

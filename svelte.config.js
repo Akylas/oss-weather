1beebee63a868d9072e84651cf991df4a360b179
farfromrefug <dev@akylas.fr>
2024-10-23 11:49:46 +0200

    chore: ci

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,31 @@
 const sveltePreprocess = require('svelte-preprocess');
-// const svelteNativePreprocessor = require('svelte-native-preprocessor');
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+
+let customPreprocess;
+const customPath = resolve('./svelte.config.custom.js');
+if (existsSync(customPath)) {
+    customPreprocess = require(customPath);
+}
+// this can be called either through svelte-loader where we want either __ANDROID__ or __IOS__ to be defined but not both
+// or through svelte-check where we want both so everything is checked
+const webpack_env = process.env['NATIVESCRIPT_WEBPACK_ENV']
+    ? JSON.parse(process.env['NATIVESCRIPT_WEBPACK_ENV'])
+    : {
+          android: true,
+          ios: true
+      };
+
 module.exports = {
     compilerOptions: {
         namespace: 'foreign'
     },
     preprocess: [
         sveltePreprocess({
+            replace: [
+                [/__ANDROID__/g, !!webpack_env.android],
+                [/__IOS__/g, !!webpack_env.ios]
+            ].concat(customPreprocess?.replace || []),
             typescript: {
                 compilerOptions: {
                     target: 'es2020'

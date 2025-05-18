2839effb766adcc6f2145e3c7dd8d99c9e4fcf99
Martin Guillon <dev@akylas.fr>
2025-05-18 18:36:48 +0200

    chore: universal link support

---
+++ app/utils/http.ts
@@ -0,0 +1,65 @@
+export function queryString(params: { [k: string]: any }, location: string): string {
+    const obj = {};
+    let i, len, key, value;
+
+    if (typeof params === 'string') {
+        value = location.match(new RegExp('[?&]' + params + '=?([^&]*)[&#$]?'));
+        return value ? value[1] : undefined;
+    }
+
+    const locSplit = location.split(/[?&]/);
+    // _params[0] is the url
+
+    const parts = [];
+    for (i = 0, len = locSplit.length; i < len; i++) {
+        const theParts = locSplit[i].split('=');
+        if (!theParts[0]) {
+            continue;
+        }
+        if (theParts[1]) {
+            parts.push(theParts[0] + '=' + theParts[1]);
+        } else {
+            parts.push(theParts[0]);
+        }
+    }
+    if (Array.isArray(params)) {
+        let data;
+
+        for (i = 0, len = params.length; i < len; i++) {
+            data = params[i];
+            if (typeof data === 'string') {
+                parts.push(data);
+            } else if (Array.isArray(data)) {
+                parts.push(data[0] + '=' + data[1]);
+            }
+        }
+    } else if (typeof params === 'object') {
+        for (key in params) {
+            value = params[key];
+            if (typeof value === 'undefined') {
+                delete obj[key];
+            } else {
+                if (typeof value === 'object') {
+                    obj[key] = JSON.stringify(value);
+                } else {
+                    obj[key] = value;
+                }
+            }
+        }
+        for (key in obj) {
+            parts.push(key + (obj[key] === true ? '' : '=' + obj[key]));
+        }
+    }
+
+    return parts.splice(0, 2).join('?') + (parts.length > 0 ? '&' + parts.join('&') : '');
+}
+
+export function parseUrlQueryParameters(url: string) {
+    const regex = /[?&]([^=#]+)=([^&#]*)/g,
+        params: Record<string, string> = {};
+    let match;
+    while ((match = regex.exec(url))) {
+        params[match[1]] = match[2];
+    }
+    return params;
+}

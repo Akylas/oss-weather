62a942fbb886954553320ab3e11247f8118ca4ca
Martin Guillon <dev@akylas.fr>
2025-10-01 14:44:05 +0200

    chore: lint

--- app/components/settings/Settings.svelte
+++ app/components/settings/Settings.svelte
@@ -1326,7 +1326,7 @@
             </Template>
             <Template key="switch" let:item>
                 <ListItemAutoSize fontSize={20} item={{ ...item, title: getTitle(item), subtitle: getDescription(item) }} leftIcon={item.icon} on:tap={(event) => onTap(item, event)}>
-                    <switch id="checkbox" checked={item.value} col={1} marginLeft={10} on:checkedChange={(e) => onCheckBox(item, e)} ios:backgroundColor={colorPrimary} verticalAlignment="center" />
+                    <switch id="checkbox" checked={item.value} col={1} marginLeft={10} verticalAlignment="center" on:checkedChange={(e) => onCheckBox(item, e)} ios:backgroundColor={colorPrimary} />
                 </ListItemAutoSize>
             </Template>
             <Template key="checkbox" let:item>
